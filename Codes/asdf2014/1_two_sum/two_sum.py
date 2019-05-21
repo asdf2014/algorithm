367e716ab9f6e034394958f2e0db2e8b59ff1969
asdf2014 <asdf2014@apache.org>
2019-05-21 21:00:59 +0800

    Add more docs for two_sum

--- Codes/asdf2014/1_two_sum/two_sum.py
+++ Codes/asdf2014/1_two_sum/two_sum.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/two-sum/
 
 
+# 从 nums 中找到两个数，满足两数之和为 target
 def two_sum(nums, target):
     nums_len = len(nums)
     if nums_len < 1:
@@ -9,8 +10,10 @@ def two_sum(nums, target):
     for i in range(nums_len):
         n = nums[i]
         if n in cache:
+            # 2. 当前值 n 在 Map 中找到对应的 key 则拿出对应的 value，因为此时 n + key 刚好等于 target
             return [cache[n], i]
         else:
+            # 1. 当前值 n 与目标值 target 的差值作为 Map 的 key，而下标作为 value
             cache[target - n] = i
 
 
