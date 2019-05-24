2191ceb476efcff993fb3d36cfb8f53e6f7ab759
XellossRyan <lp8800lz@gmail.com>
2019-05-24 22:38:40 +0800

    add java file solve 2

---
+++ Codes/GinRyan/LeetCode/Java 8 Solves/LeeCodesSolves/srcs/org/ginryan/github/leetcodes/s2_add_two_numbers/Solution.java
@@ -0,0 +1,45 @@
+package org.ginryan.github.leetcodes.s2_add_two_numbers;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 
+ * 示例：
+ * 
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 
+ * 输出：7 -> 0 -> 8
+ * 
+ * 原因：342 + 465 = 807
+ *
+ * @author GinRyan
+ */
+class Solution {
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode listNode = new ListNode(0);
+		
+		
+		
+		return listNode.next;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
