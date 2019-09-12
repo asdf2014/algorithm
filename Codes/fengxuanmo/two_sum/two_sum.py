c75b2c05d05a88c02213471ec0681aecb7d60e00
fengkai <fengkai@megvii.com>
2019-09-12 17:51:18 +0800

    two_sum add by fxm

---
+++ Codes/fengxuanmo/two_sum/two_sum.py
@@ -0,0 +1,18 @@
+#! coding:utf-8
+
+
+def twoSum(nums, target):
+    """
+    用哈希的办法，
+    valuer作为下标，key为target-num1
+    """
+    if nums is None:
+        return False
+    else:
+        m = {}
+        for i, j in enumerate(nums):
+            if j in m:
+                return [m[j], i]
+            else:
+                m[target - j] = i
+
