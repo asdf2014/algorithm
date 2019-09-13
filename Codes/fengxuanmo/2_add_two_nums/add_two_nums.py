83340ecf4101bedc445ebb6b62f2f0219e82a12c
XuanmoFeng <763608087@qq.com>
2019-09-13 23:59:45 +0800

    增加第2题

---
+++ Codes/fengxuanmo/2_add_two_nums/add_two_nums.py
@@ -0,0 +1,18 @@
+#! coding:utf-8
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+def addTwoNumbers( l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+
+if __name__ == '__main__':
+    addTwoNumbers()
