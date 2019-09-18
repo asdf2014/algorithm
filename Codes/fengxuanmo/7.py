de38ca1e02b8a02b11044b3403499f878dfceaa3
XuanmoFeng <763608087@qq.com>
2019-09-18 14:34:32 +0800

    chage the 7th question

--- Codes/fengxuanmo/7.py
+++ Codes/fengxuanmo/7.py
@@ -1,7 +1,7 @@
 def reverse(x):
-    x= x if x > 0 else x*-1
-    x = long(str(x)[::-1])
+    x = long(str(x if x > 0 else x * -1)[::-1])
     return 0 if x > (2 ** 31 - 1) or x < -2 ** 31 else x
+
 
 if __name__ == '__main__':
     assert reverse(-923) == --329
