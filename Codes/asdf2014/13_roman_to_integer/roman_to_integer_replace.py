ae8b4de360bf3e16adabdcc3759958509b5f31fa
Benedict Jin <asdf2014@apache.org>
2019-12-15 23:05:25 +0800

    Update roman_to_integer_replace.py

--- Codes/asdf2014/13_roman_to_integer/roman_to_integer_replace.py
+++ Codes/asdf2014/13_roman_to_integer/roman_to_integer_replace.py
@@ -6,7 +6,7 @@ d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 def roman_to_integer(s):
     res = 0
-    s = s.replace("IV", "IIII").replace("IX", "VIIII")
+    s = s.replace("IV", "IIII").replace("IX", "VIIII") 
     s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
     s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
     for c in s:
