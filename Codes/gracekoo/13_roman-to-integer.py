c1c0cf6065df0e75adc32d7d674de75fd9d4779d
asdf2014 <asdf2014@apache.org>
2019-12-25 13:10:17 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/13_roman-to-integer.py
+++ Codes/gracekoo/13_roman-to-integer.py
@@ -8,9 +8,9 @@
 class Solution:
     def romanToInt(self, s):
         output = 0
-        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
+        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         for i in range(len(s)):
-            if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i+1]]:
+            if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i + 1]]:
                 output -= roman_dict[s[i]]
             else:
                 output += roman_dict[s[i]]
@@ -19,4 +19,3 @@ class Solution:
 
 so = Solution()
 print(so.romanToInt("MCMXCIV"))  # 1994
-
