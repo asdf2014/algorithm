0711c7ec3f131bf6201ef4fab18fdb7e0a0f15fa
GraceKoo <gracekoo199@gmail.com>
2019-12-27 00:01:51 +0800

    20_valid-parentheses

---
+++ Codes/gracekoo/20_valid-parentheses.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time: 2019/12/26 11:53 下午 
+# @Author: GraceKoo
+# @File: 20_valid-parentheses.py
+# @Desc:
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dic = {"{": "}", "[": "]", "(": ")"}
+        for c in s:
+            if c in dic:
+                stack.append(c)
+            elif dic[stack.pop()] != c:
+                return False
+        return len(stack) == 0
+
+
+so = Solution()
+print(so.isValid("()[]{}"))
+
