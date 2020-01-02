3d82c9450f5a9be1591474c3cf330bb8738aa2f8
GraceKoo <gracekoo199@gmail.com>
2020-01-02 23:59:45 +0800

    26_remove-duplicates-from-sorted-array.py

---
+++ Codes/gracekoo/26_remove-duplicates-from-sorted-array.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/1/2 11:53 下午
+# @Author: GraceKoo
+# @File: 26_remove-duplicates-from-sorted-array.py
+# @Desc: https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i+1
+
+
+so = Solution()
+print(so.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
