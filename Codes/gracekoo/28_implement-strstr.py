f65cd4ea039ccf3626598bb46f227092adc7acf8
asdf2014 <asdf2014@apache.org>
2020-01-04 13:29:51 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/28_implement-strstr.py
+++ Codes/gracekoo/28_implement-strstr.py
@@ -11,13 +11,13 @@ class Solution:
         if len(haystack) < len(needle):
             return -1
         j = 0
-        for i in range(0, len(haystack)-len(needle)+1):
+        for i in range(0, len(haystack) - len(needle) + 1):
             index = i
             for j in range(0, len(needle)):
                 if haystack[i] != needle[j]:
                     break
                 i += 1
-            if j == len(needle)-1:
+            if j == len(needle) - 1:
                 return index
         return -1
 
