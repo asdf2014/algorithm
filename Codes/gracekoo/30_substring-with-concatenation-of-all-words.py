2e375a0eb07b8a81ebc783be6f2cbd7a541987ae
GraceKoo <gracekoo199@gmail.com>
2020-01-16 20:17:55 +0800

    fix bug of 30_substring-with-concatenation-of-all-words.py

--- Codes/gracekoo/30_substring-with-concatenation-of-all-words.py
+++ Codes/gracekoo/30_substring-with-concatenation-of-all-words.py
@@ -12,13 +12,14 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """
-        if len(words) == 0 or s == "":
+        if len(s) == 0 or not words:
             return []
 
-        l_s = len(s)
-        n = len(words)
-        l_w = len(words[0])
-        # 创建一个字典，存储words的单词和个数
+        words_number = len(words)
+        words_len = len(words[0])
+        s_len = len(s)
+
+        # 将words中的单词与个数存储到字典中
         word_dic = {}
         for word in words:
             if word in word_dic.keys():
@@ -26,40 +27,61 @@ class Solution(object):
             else:
                 word_dic[word] = 1
 
-        res = []
-        # 不同起点开始切片
-        for i in range(l_w):
-            cut_list = []
-            start = i
-            c_n = 0
+        output_list = []
+        # 按照word的长度设三段切词的起点
+        for i in range(words_len):
+            # 将s按照word的长度截取words_number段
+            start_index = i
+
+            # 先对s进行切割
+            s_cut_list = []
+            cut_number = 0
             # 切词
+<<<<<<< HEAD
             while l_s - start >= l_w:
                 cut_list.append(s[start : start + l_w])
                 c_n += 1
                 start += l_w
+=======
+            while s_len - start_index >= words_len:
+                s_cut_list.append(s[start_index:start_index + words_len])
+                cut_number += 1
+                start_index += words_len
+>>>>>>> 708ddaf... fix bug of 30_substring-with-concatenation-of-all-words.py
 
-            start_ind = 0
-            while c_n - start_ind >= n:
+            # 再对切割后的s进行判断，检验长度要求，个数要求
+            start_cut_i = 0
+            while cut_number - start_cut_i > words_number:
+                # count记录当前已经匹配完成的个数
                 count = 0
                 w_dic = {}
+<<<<<<< HEAD
                 # 遍历查找，直到匹配完成
                 for w in cut_list[start_ind : start_ind + n]:
                     if w in word_dic.keys():
                         if w in w_dic.keys():
                             w_dic[w] += 1
                             if w_dic[w] > word_dic[w]:
+=======
+                # 在s_cut_list中截取n个单词
+                for s_word in s_cut_list[start_cut_i:start_cut_i + words_number]:
+                    if s_word in word_dic.keys():
+                        if s_word in w_dic.keys():
+                            w_dic[s_word] += 1
+                            if w_dic[s_word] > word_dic[s_word]:
+>>>>>>> 708ddaf... fix bug of 30_substring-with-concatenation-of-all-words.py
                                 break
                             count += 1
                         else:
-                            w_dic[w] = 1
+                            w_dic[s_word] = 1
                             count += 1
                     else:
                         break
-                if count == n:
-                    res.append(i + start_ind * l_w)
-                start_ind += 1
+                if count == words_number:
+                    output_list.append(i + start_cut_i * words_len)
+                start_cut_i += 1
 
-        return res
+        return output_list
 
 
 so = Solution()
