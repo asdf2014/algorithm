a9a7f390a50eda40804d0d968c177d12a44d7cdd
GraceKoo <gracekoo199@gmail.com>
2020-02-05 23:34:19 +0800

    37_sudoku-solver.py

---
+++ Codes/gracekoo/37_sudoku-solver.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/2/5 9:44 上午 
+# @Author: GraceKoo
+# @File: 37_sudoku-solver.py
+# @Desc:https://leetcode-cn.com/problems/sudoku-solver/
+from collections import defaultdict
+
+
+class Solution:
+    def solveSudoku(self, board) -> None:
+        row = [set(range(1, 10)) for _ in range(9)]  # 行剩余可用数字
+        col = [set(range(1, 10)) for _ in range(9)]  # 列剩余可用数字
+        block = [set(range(1, 10)) for _ in range(9)]  # 块剩余可用数字
+
+        empty = []  # 收集需填数位置
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':  # 更新可用数字
+                    val = int(board[i][j])
+                    row[i].remove(val)
+                    col[j].remove(val)
+                    block[(i // 3) * 3 + j // 3].remove(val)
+                else:
+                    empty.append((i, j))
+
+        def backtrack(iter=0):
+            if iter == len(empty):  # 处理完empty代表找到了答案
+                return True
+            i, j = empty[iter]
+            b = (i // 3) * 3 + j // 3
+            for val in row[i] & col[j] & block[b]:
+                row[i].remove(val)
+                col[j].remove(val)
+                block[b].remove(val)
+                board[i][j] = str(val)
+                if backtrack(iter + 1):
+                    return True
+                row[i].add(val)  # 回溯
+                col[j].add(val)
+                block[b].add(val)
+            return False
+
+        backtrack()
+
+
+so = Solution()
+print(so.solveSudoku([
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+))
