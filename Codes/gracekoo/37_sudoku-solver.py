b27b5b35c0c0cdef5828cb1a02502893d63b4a8c
GraceKoo <gracekoo199@gmail.com>
2020-02-08 21:44:02 +0800

    add common on 37_sudoku-solver.py

--- Codes/gracekoo/37_sudoku-solver.py
+++ Codes/gracekoo/37_sudoku-solver.py
@@ -8,39 +8,41 @@ from collections import defaultdict
 
 class Solution:
     def solveSudoku(self, board) -> None:
-        row = [set(range(1, 10)) for _ in range(9)]  # 行剩余可用数字
-        col = [set(range(1, 10)) for _ in range(9)]  # 列剩余可用数字
-        block = [set(range(1, 10)) for _ in range(9)]  # 块剩余可用数字
+        rows_available = [set(range(1, 10)) for _ in range(9)] # 行剩余可用数字
+        columns_available = [set(range(1, 10)) for _ in range(9)]  # 列剩余可用数字
+        boxes_available = [set(range(1, 10)) for _ in range(9)]  # 块剩余可用数字
 
-        empty = []  # 收集需填数位置
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != ".":  # 更新可用数字
-                    val = int(board[i][j])
-                    row[i].remove(val)
-                    col[j].remove(val)
-                    block[(i // 3) * 3 + j // 3].remove(val)
+        # 根据board数据统计每行每列可用的数字
+        empty = []
+        for row in range(9):
+            for column in range(9):
+                if board[row][column] == ".":
+                    empty.append((row, column))
                 else:
-                    empty.append((i, j))
+                    num = int(board[row][column])
+                    rows_available[row].remove(num)
+                    columns_available[column].remove(num)
+                    boxes_available[(row // 3) * 3 + column // 3].remove(num)
 
-        def backtrack(iter=0):
-            if iter == len(empty):  # 处理完empty代表找到了答案
+        # 回溯函数，如果当前添加的数字个数已经等于空格的个数，则证明已添加到最后一个空格，即找到答案
+        def backtrack(interation = 0):
+            if interation == len(empty):
                 return True
-            i, j = empty[iter]
-            b = (i // 3) * 3 + j // 3
-            for val in row[i] & col[j] & block[b]:
-                row[i].remove(val)
-                col[j].remove(val)
-                block[b].remove(val)
-                board[i][j] = str(val)
-                if backtrack(iter + 1):
+            i, j = empty[interation]  # 依次取出待填入空格的坐标
+            for value in rows_available[i] & columns_available[j] & boxes_available[(i // 3) * 3 + j // 3]:
+                rows_available[i].remove(value)
+                columns_available[j].remove(value)
+                boxes_available[(i // 3) * 3 + j // 3].remove(value)
+                board[i][j] = value
+                if backtrack(interation + 1):
                     return True
-                row[i].add(val)  # 回溯
-                col[j].add(val)
-                block[b].add(val)
+                # 如果没找到答案，则进行回溯
+                rows_available[i].add(value)
+                columns_available[j].add(value)
+                boxes_available[(i // 3) * 3 + j // 3].add(value)
             return False
-
         backtrack()
+        return board
 
 
 so = Solution()
