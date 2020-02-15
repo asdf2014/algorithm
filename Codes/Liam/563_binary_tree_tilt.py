452f0523342e0ff778ad6980cbea7d778fab43db
asdf2014 <asdf2014@apache.org>
2020-02-15 14:28:33 +0000

    Code Formatter & Update Leaderboard

--- Codes/Liam/563_binary_tree_tilt.py
+++ Codes/Liam/563_binary_tree_tilt.py
@@ -10,6 +10,7 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def findTilt(self, root: TreeNode) -> int:
         def sum_and_tilt(root):
@@ -17,7 +18,10 @@ class Solution:
                 return 0, 0
             sum_left, tilt_left = sum_and_tilt(root.left)
             sum_right, tilt_right = sum_and_tilt(root.right)
-            return sum_left + sum_right + root.val, abs(sum_left - sum_right) + tilt_left + tilt_right
-        
+            return (
+                sum_left + sum_right + root.val,
+                abs(sum_left - sum_right) + tilt_left + tilt_right,
+            )
+
         sum_tree, tilt_tree = sum_and_tilt(root)
         return tilt_tree
