9de4d046c0a63fa78f99b98080681fac2dd56cd7
Liam <ldin3097@uni.sydney.edu.au>
2020-02-15 23:05:56 +0800

    Create 1346_check_if_N_and_its_double_exist.py

---
+++ Codes/Liam/1346_check_if_N_and_its_double_exist.py
@@ -0,0 +1,13 @@
+# 执行用时 : 160 ms
+# 内存消耗 : 13.2 MB
+
+# 方案：挨个遍历 看看是否它的double也在数组
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        n = len(arr)
+        for i in range(n):
+            for j in range(n):
+                if i!=j and arr[i]*2 == arr[j]:
+                    return True
+        return False
