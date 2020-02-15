78fde6e690d7258a5c0cec638f5bb2cbd09bf328
asdf2014 <asdf2014@apache.org>
2020-02-15 15:03:22 +0000

    Code Formatter & Update Leaderboard

--- Codes/Liam/1323_maximum_69_number.py
+++ Codes/Liam/1323_maximum_69_number.py
@@ -3,6 +3,7 @@
 
 # 方案：最左边的6，把它替换成9
 
+
 class Solution:
-    def maximum69Number (self, num: int) -> int:
+    def maximum69Number(self, num: int) -> int:
         return int(str(num).replace("6", "9", 1))
