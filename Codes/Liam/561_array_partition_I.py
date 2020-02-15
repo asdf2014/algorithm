7d225605855e97500a0c61b1fcb6aef3308dc1a2
asdf2014 <asdf2014@apache.org>
2020-02-15 14:19:17 +0000

    Code Formatter & Update Leaderboard

--- Codes/Liam/561_array_partition_I.py
+++ Codes/Liam/561_array_partition_I.py
@@ -3,6 +3,7 @@
 
 # 方案：排序后 相邻两个最小的就是奇数
 
+
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         """
@@ -11,7 +12,7 @@ class Solution:
         """
         nums.sort()
         sum = 0
-        for i in range(0,len(nums),2):
+        for i in range(0, len(nums), 2):
             sum += nums[i]
-            
+
         return sum
