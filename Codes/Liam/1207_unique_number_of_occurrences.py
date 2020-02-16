a537f921df9013570867b4d78192ed092ad44137
Liam <ldin3097@uni.sydney.edu.au>
2020-02-16 18:23:20 +0800

    Create 1207_unique_number_of_occurrences.py

---
+++ Codes/Liam/1207_unique_number_of_occurrences.py
@@ -0,0 +1,11 @@
+# 执行用时 : 36 ms
+# 内存消耗 : 13.2 MB
+
+# 方案：哈希字典
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        from collections import Counter
+        cor_dict = Counter(arr)
+        values = cor_dict.values()
+        return True if len(values) == len(list(set(values))) else False
