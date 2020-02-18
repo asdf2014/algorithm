26b45a0bf14acf79706e58da5aeb35974db5bada
Liam <ldin3097@uni.sydney.edu.au>
2020-02-19 00:52:42 +0800

    Update 226_invert_binary_tree.py

--- Codes/Liam/226_invert_binary_tree.py
+++ Codes/Liam/226_invert_binary_tree.py
@@ -10,10 +10,17 @@
 #         self.left = None
 #         self.right = None
 
-
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         if not root:
             return None
+        # 先序遍历，从顶向下交换
         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        # #后序遍历，从下往上交换
+        # leftNode, rightNode = self.invertTree(root.left), self.invertTree(root.right)
+
+        # root.right = leftNode
+        # root.left = rightNode
+
         return root
