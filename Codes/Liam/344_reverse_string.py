ee6456f6428f13009781ca2b76d9369d8f41b5aa
Liam <ldin3097@uni.sydney.edu.au>
2020-02-20 00:58:39 +0800

    Create 344_reverse_string.py

---
+++ Codes/Liam/344_reverse_string.py
@@ -0,0 +1,11 @@
+# 执行用时 : 296 ms
+# 内存消耗 : 33.1 MB
+
+# 方案：内置字符串处理
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
