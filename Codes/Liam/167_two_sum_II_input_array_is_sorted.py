820a4905328fcf4829c3df787fe537fbe6afa760
asdf2014 <asdf2014@apache.org>
2020-02-19 13:31:30 +0000

    Code Formatter & Update Leaderboard

--- Codes/Liam/167_two_sum_II_input_array_is_sorted.py
+++ Codes/Liam/167_two_sum_II_input_array_is_sorted.py
@@ -1,5 +1,5 @@
-#执行用时 : 136 ms
-#内存消耗 : 29.2 MB
+# 执行用时 : 136 ms
+# 内存消耗 : 29.2 MB
 
 # 方案：移动指针
 
@@ -7,10 +7,10 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
-        
+
         while left < right:
             if numbers[left] + numbers[right] == target:
-                return [left+1, right+1]
+                return [left + 1, right + 1]
             elif numbers[left] + numbers[right] < target:
                 left += 1
             else:
