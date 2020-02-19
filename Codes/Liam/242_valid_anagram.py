abc57f4231fec55a8676a1cdc34260d2081ac0ce
Liam <ldin3097@uni.sydney.edu.au>
2020-02-19 21:03:56 +0800

    Create 242_valid_anagram.py

---
+++ Codes/Liam/242_valid_anagram.py
@@ -0,0 +1,16 @@
+# 执行用时 : 148 ms
+# 内存消耗 : 29.2 MB
+
+# 方案：python语句
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # pythonic 解法
+        # 执行用时 : 148 ms, 在所有 Python3 提交中击败了5.10%的用户
+        # 内存消耗 : 29.2 MB, 在所有 Python3 提交中击败了5.35%的用户
+        # from collections import Counter
+        # return Counter(s) == Counter(t)
+
+        return sorted(s) == sorted(t)
+
+        
