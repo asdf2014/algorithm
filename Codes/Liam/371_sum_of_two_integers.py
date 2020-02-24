d456fcaa8edc160b6a36a19b2cee246fe16fa0df
asdf2014 <asdf2014@apache.org>
2020-02-24 10:02:48 +0000

    Code Formatter & Update Leaderboard

--- Codes/Liam/371_sum_of_two_integers.py
+++ Codes/Liam/371_sum_of_two_integers.py
@@ -3,8 +3,9 @@
 
 # 方案：计算机组成原理
 
+
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         while b:
-            a, b = (a^b)&0xFFFFFFFF,(a&b)<<1 
+            a, b = (a ^ b) & 0xFFFFFFFF, (a & b) << 1
         return a if a >> 31 == 0 else a - 4294967296
