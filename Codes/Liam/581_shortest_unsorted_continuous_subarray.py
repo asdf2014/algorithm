509cf7553bdc7cf8600295983b84acf7dd415b2b
Liam <ldin3097@uni.sydney.edu.au>
2020-02-25 17:07:19 +0800

    Update 581_shortest_unsorted_continuous_subarray.py

--- Codes/Liam/581_shortest_unsorted_continuous_subarray.py
+++ Codes/Liam/581_shortest_unsorted_continuous_subarray.py
@@ -14,3 +14,18 @@ class Solution:
         while nums[0] == min(nums):
             nums.pop(0)
         return len(nums)
+    
+    
+# 执行用时 : 276 ms
+# 内存消耗 : 14.8 MB
+
+# 方案：排序之后，双指针进行对比
+
+        sorted_nums = sorted(nums)
+        p1 = 0
+        p2 = len(nums) - 1
+        while p1 <= p2 and sorted_nums[p1] == nums[p1]:
+            p1 += 1
+        while p1 <= p2 and sorted_nums[p2] == nums[p2]:
+            p2 -= 1
+        return p2 - p1 + 1
