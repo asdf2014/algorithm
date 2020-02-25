b2c437e2f8ea161de41e4637223bdf7e2319aa2a
asdf2014 <asdf2014@apache.org>
2020-02-25 09:05:27 +0000

    Code Formatter & Update Leaderboard

--- Codes/Liam/581_shortest_unsorted_continuous_subarray.py
+++ Codes/Liam/581_shortest_unsorted_continuous_subarray.py
@@ -3,9 +3,10 @@
 
 # 方案：不需要再排序的必然是最大值or最小值，去掉他们 就是需要再继续排序的
 
+
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        #不需要再排序的必然是最大值or最小值，去掉他们 就是需要再继续排序的
+        # 不需要再排序的必然是最大值or最小值，去掉他们 就是需要再继续排序的
         if nums == sorted(nums):
             return 0
         while nums[-1] == max(nums):
