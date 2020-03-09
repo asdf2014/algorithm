85014d085bdc16596f851e98ad4981ae1e0b5530
Liam <ldin3097@uni.sydney.edu.au>
2020-03-09 18:52:10 +0800

    Create 121_best_time_to_buy_and_sell_stock.py

---
+++ Codes/Liam/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,17 @@
+# 执行用时 : 44 ms
+# 内存消耗 : 14.5 MB
+
+# 方案：
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        # base case: dp[-1][0] = 0, dp[-1][1] = -infinity
+        dp_i_0 = 0
+        dp_i_1 = -9999999
+        for i in range(n):
+            # dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
+            # dp[i][1] = max(dp[i-1][1], -prices[i])
+            dp_i_1 = max(dp_i_1, -prices[i])
+        return dp_i_0
