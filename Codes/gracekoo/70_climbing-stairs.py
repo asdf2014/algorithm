e556f7efe098319995163cb5163919890d3ab42f
asdf2014 <asdf2014@apache.org>
2020-03-11 13:50:09 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/70_climbing-stairs.py
+++ Codes/gracekoo/70_climbing-stairs.py
@@ -11,7 +11,7 @@ class Solution:
         dp[0] = 1
         dp[1] = 2
         for i in range(2, n):
-            dp[i] = dp[i-1] + dp[i-2]
+            dp[i] = dp[i - 1] + dp[i - 2]
         return dp[-1]
 
 
