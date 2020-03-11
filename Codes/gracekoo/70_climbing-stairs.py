72978298e7aa675f8be61b5ca859228d356f2142
GraceKoo <gracekoo199@gmail.com>
2020-03-11 21:47:39 +0800

    70_climbing-stairs.py

---
+++ Codes/gracekoo/70_climbing-stairs.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/3/11 12:09
+# @Author: GraceKoo
+# @File: 70_climbing-stairs.py
+# @Desc:https://leetcode-cn.com/problems/climbing-stairs/
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [i for i in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
+
+
+so = Solution()
+print(so.climbStairs(3))
