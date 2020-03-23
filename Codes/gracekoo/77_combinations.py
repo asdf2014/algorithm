a93da76c5031e618a4fa9eedce950cd341327fa1
GraceKoo <gracekoo199@gmail.com>
2020-03-23 14:40:41 +0800

    206_reverse-linked-list.py

---
+++ Codes/gracekoo/77_combinations.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/3/22 22:32 
+# @Author: GraceKoo
+# @File: 77_combinations.py
+# @Desc:https://leetcode-cn.com/problems/combinations/
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = list()
+        return result
+
+
+so = Solution()
+print(so.combine(4, 2))
