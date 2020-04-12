6f8e602bf2dfbbcdfe15ede510e34f600d6f4bb1
asdf2014 <asdf2014@apache.org>
2020-04-12 14:40:55 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/97_interleaving-string.py
+++ Codes/gracekoo/97_interleaving-string.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time: 2020/4/12 21:47 
+# @Time: 2020/4/12 21:47
 # @Author: GraceKoo
 # @File: 97_interleaving-string.py
 # @Desc: https://leetcode-cn.com/problems/interleaving-string/
@@ -12,19 +12,19 @@ class Solution:
         len_s3 = len(s3)
         if len_s1 + len_s2 != len_s3:
             return False
-        dp = [[False for _ in range(len_s2+1)] for _ in range(len_s1+1)]
+        dp = [[False for _ in range(len_s2 + 1)] for _ in range(len_s1 + 1)]
         dp[0][0] = True
         # 初始化第一行
-        for j in range(1, len_s2+1):
-            dp[0][j] = True if s3[j-1] == s2[j-1] else False
+        for j in range(1, len_s2 + 1):
+            dp[0][j] = True if s3[j - 1] == s2[j - 1] else False
         # 初始化第一列
-        for i in range(1, len_s1+1):
-            dp[i][0] = True if s3[i-1] == s1[i-1] else False
-        for i in range(1, len_s1+1):
-            for j in range(1, len_s2+1):
-                if dp[i-1][j] and s1[i-1] == s3[i+j-1]:
+        for i in range(1, len_s1 + 1):
+            dp[i][0] = True if s3[i - 1] == s1[i - 1] else False
+        for i in range(1, len_s1 + 1):
+            for j in range(1, len_s2 + 1):
+                if dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]:
                     dp[i][j] = True
-                if dp[i][j-1] and s2[j-1] == s3[i+j-1]:
+                if dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]:
                     dp[i][j] = True
         return dp[-1][-1]
 
