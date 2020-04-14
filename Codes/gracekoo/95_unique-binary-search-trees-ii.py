5b4e24c2042fccc8b54c649cd54fccadd9b133fc
asdf2014 <asdf2014@apache.org>
2020-04-14 04:42:39 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/95_unique-binary-search-trees-ii.py
+++ Codes/gracekoo/95_unique-binary-search-trees-ii.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time: 2020/4/14 12:28 
+# @Time: 2020/4/14 12:28
 # @Author: GraceKoo
 # @File: 95_unique-binary-search-trees-ii.py
 # @Desc:https://leetcode-cn.com/problems/unique-binary-search-trees-ii/
@@ -18,13 +18,15 @@ class Solution:
     def generateTrees(self, n: int) -> List[TreeNode]:
         def generate_tree(start, end):
             if start > end:
-                return [None, ]
+                return [
+                    None,
+                ]
             all_trees = []
 
             # pick a root
-            for i in range(start, end+1):
+            for i in range(start, end + 1):
                 left_tree = generate_tree(start, i)
-                right_tree = generate_tree(i+1, end)
+                right_tree = generate_tree(i + 1, end)
                 for left in left_tree:
                     for right in right_tree:
                         current_tree = TreeNode(i)
@@ -32,4 +34,5 @@ class Solution:
                         current_tree.right = TreeNode(right)
                         all_trees.append(current_tree)
             return all_trees
+
         return generate_tree(1, n) if n else []
