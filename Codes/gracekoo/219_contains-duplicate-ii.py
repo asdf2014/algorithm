b8e801dff15e0ddd88ee8c7d9eb17571b3933d56
GraceKoo <gracekoo199@gmail.com>
2020-04-30 17:51:15 +0800

    217_contains-duplicate.py

---
+++ Codes/gracekoo/219_contains-duplicate-ii.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/4/30 17:46
+# @Author: GraceKoo
+# @File: 219_contains-duplicate-ii.py
+# @Desc: https://leetcode-cn.com/problems/contains-duplicate-ii/
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        return False
+
+
+so = Solution()
+print(so.containsNearbyDuplicate([1, 2, 3, 1, 2, 3]))
