db39ce5f62ce1973d438b4285116d90975f9e5ec
GraceKoo <gracekoo199@gmail.com>
2020-05-01 22:02:07 +0800

    219_contains-duplicate-ii.py

--- Codes/gracekoo/219_contains-duplicate-ii.py
+++ Codes/gracekoo/219_contains-duplicate-ii.py
@@ -8,8 +8,17 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        nums_sort = list()
+        nums_sort.append(nums[0])
+        for i in range(1, len(nums)):
+            print(nums_sort)
+            if nums[i] in nums_sort:
+                return True
+            elif len(nums_sort) >= k:
+                nums_sort.pop(0)
+            nums_sort.append(nums[i])
         return False
 
 
 so = Solution()
-print(so.containsNearbyDuplicate([1, 2, 3, 1, 2, 3]))
+print(so.containsNearbyDuplicate([1, 2, 3, 4, 2, 3], 2))
