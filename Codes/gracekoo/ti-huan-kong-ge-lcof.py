b2e109e65cd133c2fb41e96204cb22fa5ae41062
GraceKoo <gracekoo199@gmail.com>
2020-05-08 22:53:04 +0800

    ti-huan-kong-ge-lcof.py

---
+++ Codes/gracekoo/ti-huan-kong-ge-lcof.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time: 2020/5/8 22:38 
+# @Author: GraceKoo
+# @File: ti-huan-kong-ge-lcof.py
+# @Desc: https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+
+
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+        s_list = list()
+        for s_value in s:
+            if s_value == " ":
+                s_list.append("%20")
+            else:
+                s_list.append(s_value)
+
+        return "".join(s_list)
+
+
+so = Solution()
+print(so.replaceSpace("We are happy."))
