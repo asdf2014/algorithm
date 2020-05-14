c67c7b13a85f87aa4ad86e49a827fca215b2948e
GraceKoo <gracekoo199@gmail.com>
2020-05-14 14:49:22 +0800

    update 509_fibonacci-number.py

--- Codes/gracekoo/509_fibonacci-number.py
+++ Codes/gracekoo/509_fibonacci-number.py
@@ -7,11 +7,12 @@
 
 class Solution:
     def fib(self, N: int) -> int:
-        if N == 0:
-            return 0
-        if N == 1:
-            return 1
-        return self.fib(N - 1) + self.fib(N - 2)
+        if N <= 1:
+            return N
+        f_dict = {0: 0, 1: 1}
+        for i in range(2, N):
+            f_dict[i] = f_dict[i - 1] + f_dict[i - 2]
+        return f_dict[N - 1]
 
 
 so = Solution()
