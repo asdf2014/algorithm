145a23fbeadb4a79c33e40152cbb1b3eb0053a7a
ZZhang <zhongyan.zhang@ucdconnect.ie>
2020-05-24 23:51:59 +0100

    no.42

---
+++ Codes/ZZhang/42_Trapping_Rain_Water/Solution.java
@@ -0,0 +1,45 @@
+//Given n non-negative integers representing an elevation map where the width of
+// each bar is 1, compute how much water it is able to trap after raining. 
+//
+// 
+//The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In 
+//this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos
+// for contributing this image! 
+//
+// Example: 
+//
+// 
+//Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+//Output: 6 
+// Related Topics Array Two Pointers Stack
+
+
+import java.util.Stack;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int trap(int[] height) {
+        return monotonicStack(height);
+    }
+
+    private int monotonicStack(int[] height) {
+        if (height == null) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            while(!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int curIdx = stack.pop();
+                while (!stack.isEmpty() && height[stack.peek()] == height[curIdx]) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    int stackTop = stack.peek();
+                    ans += (Math.min(height[stackTop], height[i]) - height[curIdx]) * (i - stackTop - 1);
+                }
+            }
+            stack.add(i);
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
