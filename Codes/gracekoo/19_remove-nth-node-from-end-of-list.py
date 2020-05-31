1e27c2295a3ad5d6faf25cf8c44837dcb7ccc56b
GraceKoo <gracekoo199@gmail.com>
2020-05-31 23:50:53 +0800

    update 19_remove-nth-node-from-end-of-list.py

--- Codes/gracekoo/19_remove-nth-node-from-end-of-list.py
+++ Codes/gracekoo/19_remove-nth-node-from-end-of-list.py
@@ -13,17 +13,19 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        # 哑节点用来简化某些极端情况，例如列表中只有含有一个节点，或需要删除列表的头部
-        head_pre = ListNode(-1)
-        head_pre.next, start, end = head, head_pre, head_pre
-        # 让end节点向前走n步
-        while end and n > 0:
-            end = end.next
-        # 如果end已经走到末尾，则证明比n长，直接退出
-        if not end:
+        if not head:
             return head
-        while end.next:
-            start, end = start.next, end.next
-        # 删除倒数第n个节点
-        start.next = start.next.next
-        return head
+        # 哑节点用来简化某些极端情况，例如列表中只有含有一个节点，或需要删除列表的头部
+        dum = ListNode(0)
+        fro_head = head
+        dum.next = head
+        for i in range(n):
+            head = head.next
+        # 如果end已经走到末尾，则证明比n长，直接退出
+        if not head:
+            return fro_head.next
+        while head.next:
+            fro_head = fro_head.next
+            head = head.next
+        fro_head.next = fro_head.next.next
+        return dum.next
