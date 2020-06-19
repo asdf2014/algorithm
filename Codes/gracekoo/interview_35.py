9222b40e51cd42ef316cf41b0d03e8d5e942397b
asdf2014 <asdf2014@apache.org>
2020-06-19 02:40:43 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/interview_35.py
+++ Codes/gracekoo/interview_35.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time: 2020/6/19 10:28 
+# @Time: 2020/6/19 10:28
 # @Author: GraceKoo
 # @File: interview_35.py
 # @Desc: https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof/
@@ -7,14 +7,14 @@
 
 # Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x: int, next: "Node" = None, random: "Node" = None):
         self.val = int(x)
         self.next = next
         self.random = random
 
 
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
+    def copyRandomList(self, head: "Node") -> "Node":
         def dfs(head):
             if not head:
                 return None
@@ -25,5 +25,6 @@ class Solution:
             copy.next = dfs(head.next)
             copy.random = dfs(head.random)
             return copy
+
         visited = {}
         return dfs(head)
