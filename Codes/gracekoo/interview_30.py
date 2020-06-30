c05476f07219d24a869d099392d06e2f330c5ffa
asdf2014 <asdf2014@apache.org>
2020-06-30 07:30:18 +0000

    Code Formatter & Update Leaderboard

--- Codes/gracekoo/interview_30.py
+++ Codes/gracekoo/interview_30.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time: 2020/6/30 14:52 
+# @Time: 2020/6/30 14:52
 # @Author: GraceKoo
 # @File: interview_30.py
 # @Desc: https://www.nowcoder.com/practice/6a296eb82cf844ca8539b57c23e6e9bf?tpId=13&rp=1&ru=%2Fta%2Fcoding-interviews&
@@ -13,7 +13,7 @@ class Solution:
             return 0
         max_number = nums[0]
         for i in range(1, len(nums)):
-            nums[i] += max(nums[i-1], 0)
+            nums[i] += max(nums[i - 1], 0)
             max_number = max(max_number, nums[i])
         return max_number
 
--- Codes/gracekoo/interview_50.py
+++ Codes/gracekoo/interview_50.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time: 2020/6/30 15:22 
+# @Time: 2020/6/30 15:22
 # @Author: GraceKoo
 # @File: interview_50.py
 # @Desc: https://www.nowcoder.com/practice/623a5ac0ea5b4e5f95552655361ae0a8?tpId=13&rp=1&ru=%2Fta%2Fcoding-interviews&
