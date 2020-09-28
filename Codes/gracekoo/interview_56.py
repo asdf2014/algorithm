af97ca6fa3caba4105c4dbeebbf6ce665829345a
GraceKoo <gracekoo199@gmail.com>
2020-09-28 20:36:50 +0800

    interview_56.py

--- Codes/gracekoo/interview_56.py
+++ Codes/gracekoo/interview_56.py
@@ -15,3 +15,19 @@ class ListNode:
 class Solution:
     def deleteDuplication(self, pHead):
         # write code here
+        head = ListNode(-1)  # 指向头节点
+        p = head  # 指向不重复的结点
+        p.next = pHead
+        cur = pHead  # 负责找到重复结点
+        while cur and cur.next:
+            if cur.val != cur.next.val:
+                cur = cur.next
+                p = p.next
+            else:
+                cur_val = cur.val
+                while cur and cur.val == cur_val:
+                    cur = cur.next
+                # 不相同则令p指向不重复的元素
+                p.next = cur
+        return head.next
+
