37b323854acd574783e07cdea5895d6192cfd788
GraceKoo <gracekoo199@gmail.com>
2020-10-13 15:40:43 +0800

    interview_62.py

--- Codes/gracekoo/interview_62.py
+++ Codes/gracekoo/interview_62.py
@@ -4,6 +4,7 @@
 # @File: interview_62.py
 # @Desc: https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -13,5 +14,28 @@ class TreeNode:
 
 
 class Solution:
+    def __init__(self):
+        self.k = None
+        self.result_node = None
+
     def kthLargest(self, root: TreeNode, k: int) -> int:
-        pass
+        def dfs(root):
+            # 递归结束条件
+            if not root:
+                return
+            # 先遍历右结点
+            dfs(root.right)
+
+            # 对当前结点进行判断，如果当前k已经减到0了，则找到第k大的结点了，返回
+            if self.k == 0:
+                return
+            self.k -= 1
+            if self.k == 0:
+                self.result_node = root.val
+
+            # 再遍历左结点
+            dfs(root.left)
+
+        self.k = k
+        dfs(root)
+        return self.result_node
