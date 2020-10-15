0c7125d79755f2d9d721265a1ff2f2ddba2f1673
GraceKoo <gracekoo199@gmail.com>
2020-10-15 16:37:11 +0800

    dp for interview_67.py

--- Codes/gracekoo/interview_67.py
+++ Codes/gracekoo/interview_67.py
@@ -9,6 +9,7 @@ class Solution:
     def cuttingRope(self, n: int) -> int:
         if n < 1:
             return 0
+<<<<<<< HEAD
 
         def memorize(n):
             if n == 2:
@@ -26,6 +27,17 @@ class Solution:
 
         fn = [0 for _ in range(n + 1)]  # 用于计算已计算过的函数变量
         return memorize(n)
+=======
+        dp = [0 for _ in range(n+1)]  # 用于计算已计算过的函数变量
+        dp[1] = 1
+        dp[2] = 1
+        # 遍历dp: 即F(n)
+        for i in range(3, n+1):
+           # 求解loop，求每个F(n)的解
+            for j in range(i):
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j))
+        return dp[n]
+>>>>>>> 1d3f2ee... dp for interview_67.py
 
 
 so = Solution()
