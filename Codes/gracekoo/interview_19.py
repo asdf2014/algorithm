ae6744dd21567408032601fcb754519cc54bd032
GraceKoo <gracekoo199@gmail.com>
2020-10-21 14:35:23 +0800

    comment for interview_19.py

--- Codes/gracekoo/interview_19.py
+++ Codes/gracekoo/interview_19.py
@@ -9,11 +9,13 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # 生成器
         def spiral_matrix(r1, c1, r2, c2):
             for c in range(c1, c2 + 1):
                 yield r1, c
             for r in range(r1 + 1, r2 + 1):
                 yield r, c2
+            # 如果r1==r2,或者c1==c2证明已经行或列已有重合，上面已经输出过了，无需重复输出
             if r1 < r2 and c1 < c2:
                 for c in range(c2 - 1, c1, -1):
                     yield r2, c
@@ -25,7 +27,9 @@ class Solution:
         output = []
         r1, r2 = 0, len(matrix) - 1
         c1, c2 = 0, len(matrix[0]) - 1
+        # 控制大局（矩形范围）：r1、r2、c1、c2
         while r1 <= r2 and c1 <= c2:
+            # 用for去遍历这个生成器
             for r, c in spiral_matrix(r1, c1, r2, c2):
                 output.append(matrix[r][c])
             r1 += 1
