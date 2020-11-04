77f679736d9c6956e7484e509b2e4db83365d57a
GraceKoo <gracekoo199@gmail.com>
2020-11-04 19:01:31 +0800

    add  comment for interview_47.py

--- Codes/gracekoo/interview_47.py
+++ Codes/gracekoo/interview_47.py
@@ -10,8 +10,13 @@ class Solution:
         self.res = 0
 
     def sumNums(self, n: int) -> int:
+        # 如果n大于1，则and后面的语句继续执行，即，继续递归
         n > 1 and self.sumNums(n - 1)
+
+        # 如果已经不大于1，则程序继续执行下面的语句，进行累加
         self.res += n
+
+        # 最后返回当前累加的结果
         return self.res
 
 
--- Codes/gracekoo/test.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time: 2020/10/29 20:35
-# @Author: GraceKoo
-# @File: test.py.py
-# @Desc:
