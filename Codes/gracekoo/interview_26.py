e25fd464b90f3c41952c1aa9a351517ff6cba97c
GraceKoo <gracekoo199@gmail.com>
2020-11-04 20:45:18 +0800

    add  comment for interview_26.py

--- Codes/gracekoo/interview_26.py
+++ Codes/gracekoo/interview_26.py
@@ -16,18 +16,33 @@ class Node:
 
 
 class Solution:
-    # 中序遍历
-    def zhongxu(self, root):
-        if not root:
-            return []
-        return self.zhongxu(root.left) + [root] + self.zhongxu(root.right)
+    def __init(self):
+        self.pre = None
+        self.head = None
 
-    # 将中序遍历结果进行连接
-    def treeToDoublyList(self, root: "Node") -> "Node":
-        list_result = self.zhongxu(root)
-        if len(list_result) == 0 or len(list_result) == 1:
-            return root
-        for i in range(len(list_result) - 1):
-            list_result[i].right = list_result[i + 1]
-            list_result[i + 1].left = list_result[i]
-        return list_result[0]
+    def Convert(self, pRootOfTree):
+        # write code here
+        def dfs(current_node):
+            # 递归出口：代表到达尾结点
+            if not current_node:
+                return
+            dfs(current_node.left)
+
+            # 如果pre是空，证明当前访问的是第一个结点
+            if not self.pre:
+                self.head = current_node
+            # 如果不是空，则证明当前结点不是头结点，可进行修改
+            else:
+                self.pre.right, current_node.left = current_node, self.pre
+            # 保存当前结点为pre
+            self.pre = current_node
+
+            # 再去递归右子树
+            dfs(current_node.right)
+
+        if not pRootOfTree:
+            return
+        dfs(pRootOfTree)
+        # 最后将头结点和尾结点进行连接
+        self.pre.right, self.head.left = self.head, self.pre
+        return self.head
