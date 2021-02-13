724848fba737b2777a6374c545234749bc9bd249
asdf2014 <asdf2014@apache.org>
2021-02-13 14:55:26 +0800

    Improve the comments of LRU Cache

--- Codes/asdf2014/146_lru_cache/lru_cache.py
+++ Codes/asdf2014/146_lru_cache/lru_cache.py
@@ -1,16 +1,16 @@
 # https://leetcode-cn.com/problems/lru-cache/
 
 
-# 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
+# 运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制。
 #
 # 实现 LRUCache 类：
-#
-# LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
-# int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+# LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存。
+# int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1。
 # void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
 # 当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
 #
-# 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
+# 进阶：
+# 你是否可以在 O(1) 时间复杂度内完成这两种操作？
 #
 # 示例：
 # 输入
