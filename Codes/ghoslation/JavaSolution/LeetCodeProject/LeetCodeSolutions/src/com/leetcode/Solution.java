e966e288a9dae1809a95b90fc7f976d03623f3cd
ghoslation <ghoslation@gmail.com>
2021-03-14 23:02:13 +0900

    my first commit

---
+++ Codes/ghoslation/JavaSolution/LeetCodeProject/LeetCodeSolutions/src/com/leetcode/Solution.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] retArr = new int[2];
+        for(int i = 0;i <nums.length - 1; i++) {
+            for(int j = i + 1;j < nums.length;j++) {
+                if(nums[i] + nums[j] == target) {
+                    retArr[0] = i;
+                    retArr[1] = j;
+                    return retArr;
+                }
+            }
+        }
+        return retArr;
+    }
+}
