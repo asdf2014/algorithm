cda502511ae5996bdad8600dbd8b44253244cfa5
shengyaqingfeng <shengyaqingfeng@gmail.com>
2022-10-02 10:45:05 +0800

    auto find files and config CMakeLists

--- Codes/shengyaqingfeng/CalcNextSat.py
+++ Codes/shengyaqingfeng/CalcNextSat.py
@@ -2,6 +2,8 @@
 import os
 from datetime import datetime, timedelta
 
+project_path = "leetcode/editor/cn/"
+
 
 def next_weekday(weekday, d=datetime.now()):
     delta = weekday - d.isoweekday()
@@ -15,6 +17,19 @@ def parse_ymd(s):
     return datetime(int(year_s), int(mon_s), int(day_s))
 
 
+def get_latest_file():
+    files = os.listdir(project_path)
+    max_index = 0
+    result = ""
+    for file in files:
+        if file[0] == "[":
+            file_index = int(file[1:file.find("]")])
+            if file_index > max_index:
+                max_index = file_index
+                result = file
+    return result
+
+
 with open("README.md", "r", encoding="utf-8") as fp:
     lines = fp.readlines()
     last_line = lines[-1]
@@ -24,12 +39,10 @@ with open("README.md", "r", encoding="utf-8") as fp:
     # date
     time_str[3] = datetime.now().strftime("%Y-%m-%d")
     # Title
-    next_file_name = "[14]LongestCommonPrefix.cpp"
-    q_index = next_file_name[1 : next_file_name.find("]")]
-    q_title = next_file_name[next_file_name.find("]") + 1 :]
-    time_str[4] = "|[{}.{}](leetcode/editor/cn/{})|".format(
-        q_index, q_title, next_file_name
-    )
+    next_file_name = get_latest_file()
+    q_index = next_file_name[1: next_file_name.find("]")]
+    q_title = next_file_name[next_file_name.find("]") + 1:]
+    time_str[4] = "|[{}.{}]({}{})|".format(q_index, q_title, project_path, next_file_name)
     # ms
     time_str[5] = "xx"
     # MB
@@ -40,3 +53,5 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 with open(file="README.md", mode="a") as af:
     af.writelines(info)
+with open(file=project_path + "CMakeLists.txt", mode="a") as af:
+    af.writelines("add_executable({} {})".format(q_title[:-4], next_file_name))
