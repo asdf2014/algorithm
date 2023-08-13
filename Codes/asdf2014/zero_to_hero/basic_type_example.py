2c6dcc29521478cac5350a8eef55c46dcc337aa2
asdf2014 <asdf2014@apache.org>
2023-08-13 20:11:48 +0800

    Add example for basic type in Python

---
+++ Codes/asdf2014/zero_to_hero/basic_type_example.py
@@ -0,0 +1,27 @@
+i = 1
+print(i)
+print(type(i))
+
+i = 0.1
+print(i)
+print(type(i))
+
+f = 0.1
+print(f)
+print(type(f))
+
+f = 1
+print(f)
+print(type(f))
+
+s = 'yuzhouwan'
+print(s)
+print(type(s))
+
+s = 123
+print(s)
+print(type(s))
+
+s = None
+print(s)
+print(type(s))
